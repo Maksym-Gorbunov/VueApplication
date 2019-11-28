e52d92d4ec3dd29314f1298ca3ecc8146ddedf5f
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-11-28 18:07:00 +0100

    imgUrl added to imageItem

--- src/components/VideoGroup.vue
+++ src/components/VideoGroup.vue
@@ -1,16 +1,28 @@
 <template>
   <div id="videoGroup">
     <h2>VideoGroup</h2>
-
+    <div v-for="video in videos" :key="video.id">
+      <VideoItem v-bind:video="video" />
+    </div>
   </div>
 </template>
 
 <script>
-// import VideoGroup from "../VideoGroup";
+import { mapGetters, mapActions } from "vuex";
+import VideoItem from "./VideoItem";
 
 export default {
   name: "VideoGroup",
-  // components: { VideoGroup }
+  components: { VideoItem },
+  computed: mapGetters(["videos"]),
+  methods: {
+    ...mapActions(["testAction"]),
+  test(){
+    this.testAction();
+  }
+  },
+  created(){
+  }
 };
 </script>
 
